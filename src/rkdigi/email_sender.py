f730464dea48b95965d7939ac2472a6cda85a3ba
Rune Aagaard Keena <Rune.Aagaard.Keena@randers.dk>
2026-02-09 11:48:09 +0100

    moved building message object to private method.

--- src/rkdigi/email_sender.py
+++ src/rkdigi/email_sender.py
@@ -38,6 +38,81 @@ class EmailSender:
         except Exception:
             return False
 
+    def _build_message(
+        self,
+        from_addr: str,
+        recipients: str | Sequence[str] | None,
+        subject: str,
+        body: str,
+        cc: str | Sequence[str] | None,
+        attachments: Sequence[str | tuple[str, bytes]] | None
+    ) -> tuple[MIMEMultipart, str, str]:
+        if recipients is None:
+            recipients = []
+        if cc is None:
+            cc = []
+        if attachments is None:
+            attachments = []
+
+        recipients_list = [recipients] if isinstance(recipients, str) \
+            else list(recipients)
+        cc_list = [cc] if isinstance(cc, str) else list(cc)
+
+        to_addrs = recipients_list + cc_list
+
+        msg = MIMEMultipart()
+        msg["From"] = from_addr
+        msg["Subject"] = subject or ""
+
+        if recipients_list:
+            msg["To"] = ", ".join(recipients_list)
+
+        if cc_list:
+            msg["Cc"] = ", ".join(cc_list)
+
+        body_part = MIMEText(body, "html") \
+            if body and "<html>" in body.lower() \
+            else MIMEText(body or " ", "plain")
+
+        msg.attach(body_part)
+
+        for att in attachments:
+            if isinstance(att, str):
+                # If value in attachments is a string,
+                # treat it as a file path
+                with open(att, "rb") as f:
+                    part = MIMEBase("application", "octet-stream")
+                    part.set_payload(f.read())
+                encoders.encode_base64(part)
+                filename = os.path.basename(att)
+                part.add_header(
+                    "Content-Disposition",
+                    f'attachment; filename="{filename}"'
+                )
+                msg.attach(part)
+            elif (isinstance(att, tuple)
+                    and len(att) == 2
+                    and isinstance(att[0], str)
+                    and isinstance(att[1], bytes)):
+                # If value in attachments is
+                # a tuple of (filename, content)
+                # then only 'bytes' content is supported
+                filename, content = att
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(content)
+                encoders.encode_base64(part)
+                part.add_header(
+                    "Content-Disposition",
+                    f'attachment; filename="{filename}"'
+                )
+                msg.attach(part)
+            else:
+                raise ValueError(
+                    "Attachments must be file paths "
+                    "or (filename, content) tuples."
+                )
+        return msg, from_addr, to_addrs
+
     def send_email(
         self,
         sender: str = "",
@@ -47,19 +122,11 @@ class EmailSender:
         cc: str | Sequence[str] | None = None,
         attachments: Sequence[str | tuple[str, bytes]] | None = None
     ) -> None:
-        if recipients is None:
-            recipients = []
-        if cc is None:
-            cc = []
-        if attachments is None:
-            attachments = []
         with smtplib.SMTP(
             host=self._smtp_server,
             port=self._smtp_port
         ) as server:
-            server.ehlo()
             server.starttls()
-            server.ehlo()
             if self.sender_email and self._sender_password:
                 if sender:
                     raise ValueError(
@@ -75,69 +142,20 @@ class EmailSender:
             else:
                 from_addr = sender or self.sender_email
 
-            recipients_list = [recipients] if isinstance(recipients, str) \
-                else list(recipients)
-            cc_list = [cc] if isinstance(cc, str) else list(cc)
-
-            to_addrs = recipients_list + cc_list
-
-            if not from_addr or not to_addrs:
+            if not from_addr or not recipients and not cc:
                 raise ValueError(
                     "A sender and at least one recipient "
                     "(recipients or cc) must be specified."
                 )
 
-            msg = MIMEMultipart()
-            msg["From"] = from_addr
-            msg["Subject"] = subject or ""
-
-            if recipients_list:
-                msg["To"] = ", ".join(recipients_list)
-
-            if cc_list:
-                msg["Cc"] = ", ".join(cc_list)
-
-            body_part = MIMEText(body, "html") \
-                if body and "<html>" in body.lower() \
-                else MIMEText(body or " ", "plain")
-
-            msg.attach(body_part)
-
-            for att in attachments:
-                if isinstance(att, str):
-                    # If value in attachments is a string,
-                    # treat it as a file path
-                    with open(att, "rb") as f:
-                        part = MIMEBase("application", "octet-stream")
-                        part.set_payload(f.read())
-                    encoders.encode_base64(part)
-                    filename = os.path.basename(att)
-                    part.add_header(
-                        "Content-Disposition",
-                        f'attachment; filename="{filename}"'
-                    )
-                    msg.attach(part)
-                elif (isinstance(att, tuple)
-                      and len(att) == 2
-                      and isinstance(att[0], str)
-                      and isinstance(att[1], bytes)):
-                    # If value in attachments is
-                    # a tuple of (filename, content)
-                    # then only 'bytes' content is supported
-                    filename, content = att
-                    part = MIMEBase("application", "octet-stream")
-                    part.set_payload(content)
-                    encoders.encode_base64(part)
-                    part.add_header(
-                        "Content-Disposition",
-                        f'attachment; filename="{filename}"'
-                    )
-                    msg.attach(part)
-                else:
-                    raise ValueError(
-                        "Attachments must be file paths "
-                        "or (filename, content) tuples."
-                    )
+            msg, from_addr, to_addrs = self._build_message(
+                from_addr=from_addr,
+                recipients=recipients,
+                subject=subject,
+                body=body,
+                cc=cc,
+                attachments=attachments
+            )
 
             server.sendmail(
                 from_addr=from_addr,
@@ -155,7 +173,6 @@ class EmailSender:
         attachments: Sequence[str | tuple[str, bytes]] = []
     ) -> None:
         import aiosmtplib
-        msg = MIMEMultipart()
         if self.sender_email and self._sender_password:
             if sender:
                 raise ValueError(
@@ -166,67 +183,27 @@ class EmailSender:
         else:
             from_addr = sender or self.sender_email
 
-        recipients_list = [recipients] if isinstance(recipients, str) \
-            else list(recipients)
-        cc_list = [cc] if isinstance(cc, str) else list(cc)
-        to_addrs = recipients_list + cc_list
-
-        if not from_addr or not to_addrs:
+        if not from_addr or not recipients and not cc:
             raise ValueError(
                 "A sender and at least one recipient "
                 "(recipients or cc) must be specified."
             )
 
-        msg["From"] = from_addr
-        msg["Subject"] = subject or ""
-        if recipients_list:
-            msg["To"] = ", ".join(recipients_list)
-        if cc_list:
-            msg["Cc"] = ", ".join(cc_list)
-
-        body_part = MIMEText(body, "html") \
-            if body and "<html>" in body.lower() \
-            else MIMEText(body or " ", "plain")
-        msg.attach(body_part)
-
-        for att in attachments:
-            if isinstance(att, str):
-                with open(att, "rb") as f:
-                    part = MIMEBase("application", "octet-stream")
-                    part.set_payload(f.read())
-                encoders.encode_base64(part)
-                filename = os.path.basename(att)
-                part.add_header(
-                    "Content-Disposition",
-                    f'attachment; filename="{filename}"'
-                )
-                msg.attach(part)
-            elif (
-                isinstance(att, tuple)
-                and len(att) == 2
-                and isinstance(att[0], str)
-                and isinstance(att[1], bytes)
-            ):
-                filename, content = att
-                part = MIMEBase("application", "octet-stream")
-                part.set_payload(content)
-                encoders.encode_base64(part)
-                part.add_header(
-                    "Content-Disposition",
-                    f'attachment; filename="{filename}"'
-                )
-                msg.attach(part)
-            else:
-                raise ValueError(
-                    "Attachments must be file paths "
-                    "or (filename, content) tuples."
-                )
+        msg, from_addr, to_addrs = self._build_message(
+            from_addr=from_addr,
+            recipients=recipients,
+            subject=subject,
+            body=body,
+            cc=cc,
+            attachments=attachments
+        )
 
         smtp_kwargs = dict(
             hostname=self._smtp_server,
             port=self._smtp_port,
             start_tls=True,
         )
+
         if self.sender_email and self._sender_password:
             smtp_kwargs["username"] = self.sender_email
             smtp_kwargs["password"] = self._sender_password
